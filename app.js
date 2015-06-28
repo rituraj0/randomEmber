b43ffd57f2df66a0959912d459f2354dd7d2c7bd
rituraj0 <rituraj.tc@gmail.com>
2015-06-28 22:28:03 +0530

    random working

--- app.js
+++ app.js
@@ -8,18 +8,40 @@ MyApp.Router.map( function() {
 
 MyApp.FirstRouteRoute = Ember.Route.extend( {
 
-	actions: {
-		 headerClicked: function(){
 
-		 	lastname: 'done'
-		 	alert("headerClicked");
-		 }
-	}
 });
 
 MyApp.FirstRouteController = Ember.Controller.extend( {
 
-	firstname: 'hello'
+	firstname: 'hello',
+
+	dicenum: '10',
+
+	outputs: function() {
+
+		var randnum = [];
+
+		var tp=this.get('dicenum');
+		console.log("in controller");
+		console.log(tp);
+
+		if( tp == undefined)
+			 tp=5;
+		for( var x =1;x<=tp;x++)
+		{
+			randnum.push( Math.random() );
+		}
+
+		console.log(tp);
+
+		for( var x =0 ; x< tp ;x++)
+		{
+			console.log( randnum[x] );
+		}
+
+        return randnum;
+     }.property('dicenum')//if dicenum is changes , then reclalcuate outputs , watch vedio 18
+
 });
 
 MyApp.IndexRoute = Ember.Route.extend( {
